26c7cc9bdce6f93c5dea78c8f36b33cbb383e8b4
idsuarezl <idsuarezl@unal.edu.co>
2016-10-24 21:05:47 -0500

    Danilo
    Subi el codigo que hizo jairo, le cambie el asterisco y le cree el
    metodo toString, lo del main es solo para que miren como se vería cada
    ficha

--- JavaLaberinto/src/BussinessLogic/JavaLaberinto.java
+++ JavaLaberinto/src/BussinessLogic/JavaLaberinto.java
@@ -5,11 +5,22 @@
  */
 package BussinessLogic;
 
+import Data.Ficha;
+import static Data.Ficha.setFicha;
 import Data.Tablero;
 
 public class JavaLaberinto {
 
     public static void main(String[] args) {
+        Ficha ficha = new Ficha();
+        setFicha(ficha.fichaRecta());
+        System.out.println(ficha.toString());
+        Ficha ficha1 = new Ficha();
+        setFicha(ficha.fichaT());
+        System.out.println(ficha.toString());
+        Ficha ficha2 = new Ficha();
+        setFicha(ficha.fichaEsquina());
+        System.out.println(ficha.toString());
         Tablero tablero = new Tablero();
     }
     
